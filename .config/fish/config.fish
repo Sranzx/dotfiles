ecfa72660891c65cc7393c04ceb2c77b9c50e8ca
Efe <86563537+Sranzx@users.noreply.github.com>
2025-12-23 08:07:38 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-   fish_add_path /Users/tuncayalkis/.cargo/bin/
    alias sl="eza --all --color=always --group-directories-first --icons"
    alias ll="eza -al --color=always --group-directories-first --icons"
    alias ls="eza --all --color=always --group-directories-first --icons"
