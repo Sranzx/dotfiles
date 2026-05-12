951631385a886779b51d93aea5b76b0384325d4b
Efe <86563537+Sranzx@users.noreply.github.com>
2026-05-12 21:27:52 +0300

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 if status is-interactive
-   alias sl="eza --all --color=always --group-directories-first --icons"
-   alias ll="eza -al --color=always --group-directories-first --icons"
-   alias ls="eza --all --color=always --group-directories-first --icons"
+  fish_add_path /usr/local/sbin/ 
+  fish_add_path /Users/tuncayalkis/.cargo/bin/
+  alias sl="eza --all --color=always --group-directories-first --icons"
+  alias ll="eza -al --color=always --group-directories-first --icons"
+  alias ls="eza --all --color=always --group-directories-first --icons"
+  alias rm="rm -i"
 end
 
 set fish_greeting ""
+export PATH="$HOME/.local/bin:$PATH"
